66dcdd0bf53466c51a76ab726e5c5d926120d22a
Duke <dukemorise33@gmail.com>
2023-08-08 22:23:44 -0700

    competed arrow functions lab phase-1

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const divide = (a,b) => {
+
+    const dividedValue = a/b;
+    return dividedValue;
+
+}
+
+const square = (a) => {
+    const squared = a*a;
+    return squared;
+}
+
+const add = (a,b) => {
+    const sum= a+b;
+    return sum;
+}
